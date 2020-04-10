6906d91d34bef2f51ecd881e489c2129e6e25778
Astik Singh <astik.singh_cs18@gla.ac.in>
2020-04-10 22:42:55 +0530

    Created greet method.

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -14,7 +14,7 @@ public class SimpleBot {
         end();
     }
 
-    private static void greet(String assistantName, String birthYear) {
+    private static void greet(String assistantName, String birthYear)  {
         System.out.println("Hello! My name is " + assistantName + ".");
         System.out.println("I was created in " + birthYear + ".");
         System.out.println("Please, remind me your name.");
